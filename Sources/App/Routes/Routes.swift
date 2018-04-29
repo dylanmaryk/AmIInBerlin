c65d31619c02be932cd08e8dd689e52d52134a15
Dylan Maryk <dylan@door2door.io>
2018-04-29 17:22:32 +0200

    Create API

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,25 +1,32 @@
 import Vapor
 
 extension Droplet {
+    
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        get("am_i_in_berlin", ":lat", ":lng") { req in
+            guard let lat = req.parameters["lat"]?.double,
+                let lng = req.parameters["lng"]?.double else {
+                    throw Abort.badRequest
+            }
+            guard let key = self.config["keys", "google-maps-api-key"]?.string else {
+                throw Abort.serverError
+            }
+            let res = try self.client.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=\(lat),\(lng)&key=\(key)")
+            let results = res.json?["results"]?.array
+            let firstResult = results?.first?.object
+            let addressComponents = firstResult?["address_components"]?.array
+            let isInBerlin = addressComponents?.contains { addressComponent -> Bool in
+                let isLocality = addressComponent.object?["types"]?.array?.contains("locality") ?? false
+                let isBerlin = addressComponent.object?["long_name"] == "Berlin"
+                return isLocality && isBerlin
+                } ?? false
+            let isInGermany = addressComponents?.contains { addressComponent -> Bool in
+                let isCountry = addressComponent.object?["types"]?.array?.contains("country") ?? false
+                let isGermany = addressComponent.object?["long_name"] == "Germany"
+                return isCountry && isGermany
+                } ?? false
+            return String(isInBerlin && isInGermany)
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
     }
+    
 }
